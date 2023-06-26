1dccebe6fa9dbf72534e7b2377c19b29067d9c24
Adnan <adnanvandyck@gmail.com>
2023-06-26 19:31:58 -0400

    fixed the links for the images

--- front-end-ocho-3/src/App.js
+++ front-end-ocho-3/src/App.js
@@ -1,9 +1,47 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Axios from 'axios';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./style.css";
 
 function App() {
+  const [listOfPerformances, setListOfPerformances] = useState([]);
+
+
+  useEffect(() => {
+Axios.get("http://localhost:8888/performances").then((response) => {
+  setListOfPerformances(response.data)
+})
+  }, [])
+
+
+
+
   return (
     <div className="App">
-<h1> Welcome to OchoMania! </h1>
-    </div>
+      <div className="performanceDisplay">
+        {listOfPerformances.map((performance) => {
+          
+          return (
+            <div className="performanceList"> 
+              <h1>Venue: {performance.venue}</h1>
+              <h1>Event Promo:
+                <img src={performance.image} alt={performance.date} />
+                </h1>
+              <h1>Date: {performance.date}</h1>
+              <h1>Song of the Night!: {performance.bestSong} by {performance.songArtist}</h1>
+            </div>
+          )
+        }
+        )}
+      </div>
+
+
+
+
+      {/* ends "App" div */}
+    </div> 
   );
 }
 
---
+++ front-end-ocho-3/src/components/createUser.js
@@ -0,0 +1,24 @@
+
+
+// const [venue, setVenue] = useState("")
+
+// {/* <div>
+//   <input type="text" placeholder="Venue..." onChange={(e) => {
+//     setName(e.target.value) }}/>
+//   <input type="text" placeholder="Promo Image..." />
+//   <input type="date" placeholder="Date..." />
+//   <input type="text" placeholder="Song of the Night..." />
+//   <input type="text" placeholder="Song by..." />
+//   <button omClick={createPerformance()}> Create Performance</button>
+// </div> */}
+
+// const createPerformance = () => {
+//     Axios.post("http://localhost:8888/createPerformance", {
+//       venue: "", 
+//       image: "", 
+//       date: "", 
+//       bestSong: "", 
+//       songArtist: ""}).then((response) => {
+//       alert("Performance created!");
+//     });
+//   };
